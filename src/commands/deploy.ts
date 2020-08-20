8c85f6e05005868e7d9604875125ca8645b6eabe
Marton Lederer <martondeveloper@gmail.com>
2020-08-20 13:19:03 +0200

    deploy: add recommended status command

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -83,6 +83,7 @@ export default async function command({ dir, keyfile }: Record<string, string>) 
 
     log("Deployed to Arweave. Your site will be hosted on the URL below:", LogType.success);
     log(`${ "\x1b[36m" }https://arweave.net/${ manifestTransaction.id }`, LogType.log);
+    log("You can check the status of this deployment by running " + "\x1b[1m" + `verto status ${ manifestTransaction.id }`, LogType.warning)
 
   }else log("There was an error uploading your site!", LogType.error)
 
