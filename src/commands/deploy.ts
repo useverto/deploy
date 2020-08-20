47b31b81ec69a8e30cb43ce918b6e003b3a26dbe
Marton Lederer <martondeveloper@gmail.com>
2020-08-20 11:49:42 +0200

    deploy: map files

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -8,7 +8,7 @@ export default async function command({ dir, keyfile }: Record<string, string>) 
   if(dir === undefined || keyfile === undefined) return log("Insufficient options!", LogType.error);
 
   const 
-    deployDir = path.isAbsolute(dir) ? dir : path.join(process.cwd(), dir),
+    deployDir = path.isAbsolute(dir) ? dir.replace(/(\/)$/, "") : path.join(process.cwd(), dir).replace(/(\/)$/, ""),
     keyfileLocation = path.isAbsolute(keyfile) ? keyfile : path.join(process.cwd(), keyfile);
 
   if(!fs.existsSync(deployDir) || !fs.existsSync(keyfileLocation)) return log("Keyfile or deploy directory does not exist!", LogType.error);
@@ -16,6 +16,17 @@ export default async function command({ dir, keyfile }: Record<string, string>) 
   if(!fs.lstatSync(keyfileLocation).isFile()) return log("Given keyfile location does not point to a file!", LogType.error);
   if(!keyfileLocation.match(/(\.json)$/)) return log("Given keyfile is not a JSON!", LogType.error);
 
-  const keyfileContent = new TextDecoder().decode(await promises.readFile(keyfileLocation));
+  let filesToDeploy: string[] = [];
+  const 
+    keyfileContent = JSON.parse(new TextDecoder().decode(await promises.readFile(keyfileLocation))),
+    mapFiles = (analyizeDir: string) => {
+      for(const element of fs.readdirSync(analyizeDir)) {
+        if(fs.lstatSync(analyizeDir + "/" + element).isDirectory()) mapFiles(analyizeDir + "/" + element);
+        else if(fs.lstatSync(analyizeDir + "/" + element).isFile()) filesToDeploy.push(analyizeDir + "/" + element);
+      }
+    };
+
+  mapFiles(deployDir);
+  console.log(filesToDeploy);
 
 }
